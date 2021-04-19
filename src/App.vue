95c327117d7d49d28c772f9a5525a562a883a2c0
m.ivaskevych <m.ivaskevych@viseven.com>
2021-04-19 10:22:51 +0300

    sorted styles

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,28 @@
     <div class="container">
       <router-view></router-view>
     </div>
+
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+
 export default {
   name: "App",
   components: {
-    Header,
+    Header
+  },
+  data() {
+    return {
+      isScrolled: false
+    }
   },
   beforeCreate() {
     this.$store.commit("INIT_LIKED_BREEDS");
+  },
+  mounted () {
+    console.log(window.innerHeight + window.screenY)
   },
 };
 </script>
